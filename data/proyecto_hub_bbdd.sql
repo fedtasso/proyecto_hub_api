5592d3f7cd155d7733597ea06e16eee8835ed829
fedtasso <fedtasso@gmail.com>
2024-08-03 00:14:53 -0300

    CRUD usuarios

---
+++ data/proyecto_hub_bbdd.sql
@@ -0,0 +1,73 @@
+-- DROP DATABASE proyecto_hub
+
+CREATE DATABASE IF NOT EXISTS proyecto_hub;
+USE proyecto_hub;
+
+
+CREATE TABLE usuarios(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(50) NOT NULL,
+    `apellido` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(100) UNIQUE,
+    `password` CHAR(60),
+    `usuario_created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `usuario_updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
+
+
+
+CREATE TABLE informacion(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `usuario` INT, 
+    `informacion_adicional` TEXT,
+    `image` VARCHAR(255),
+    `informacion_updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE perfiles(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `usuario` INT, 
+    `perfil` VARCHAR (50),
+    `perfil_updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE lenguajes(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `usuario` INT, 
+    `lenguaje` VARCHAR (50),
+    `nivel` INT,
+    `lenguaje_updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
+
+
+-- Añadir restricciones de clave foránea
+
+ALTER TABLE informacion 
+ADD CONSTRAINT fk_informacion_usuarios 
+FOREIGN KEY (usuario) REFERENCES usuarios(id);
+
+ALTER TABLE perfiles 
+ADD CONSTRAINT fk_perfiles_usuarios 
+FOREIGN KEY (usuario) REFERENCES usuarios(id);
+
+ALTER TABLE lenguajes 
+ADD CONSTRAINT fk_lenguajes_usuarios 
+FOREIGN KEY (usuario) REFERENCES usuarios(id);
+
+
+-- Pruebas
+INSERT INTO `usuarios` VALUES (0,'Federico',  'Tasso', 'fedtasso@gmail.com','abcd1234', NOW(), NOW()),
+(0,'Bautista',  'Nar', 'bauti@gmail.com','abcd1234', NOW(), NOW());
+
+SELECT * FROM usuarios;
+
+
+-- CRUD usuarios
+SELECT * FROM usuarios where nombre = "Federico" AND apellido = "Tasso";
+INSERT INTO `usuarios` VALUES (0,'Federico',  'Tasso', 'fedtasso@gmail.com','abcd1234', NOW(), NOW())
+
--- src/app.py
+++ src/app.py
@@ -8,26 +8,152 @@ app=Flask(__name__)
 conexion=MySQL(app)
 
 
-@app.route('/usuario', methods=["GET"])
-def datos_usuario():
-    nombre = request.args.get('nombre')
-    apellido = request.args.get('apellido')
+# faltan todas las validaciones en cada endpoint
+# verificar si la peticion existe o no en la bbdd antes de ejecutarla
+
+#los get deberian ir en relacion a lo que queremos mostrar en cada pagina y no en relacion a las tablas?
+#no hice get de informacion, perfiles y lenguajes
+
+
+# ------- usuario nuevo -----------
+
+@app.route('/registrar', methods=["POST"]) #como se maneja el registro? llega todo en un solo json, son varios pasos? ver tema de tablas uno a muchos
+def registrar():
+    datos= request.get_json()
+    username = datos.get('username') 
+    fullname = datos.get('fullname')
+    email = datos.get('email')
+    password = datos.get('password')
+    informacion_adicional = datos.get('informacion_adicional')
+    image = datos.get('image')
+    
+    try: 
+        cursor=conexion.connection.cursor()
+        conexion.connection.autocommit(False)
+        #Insertar en tabla usuarios
+        sql = "INSERT INTO usuarios (username, fullname, email, password) VALUES (%s, %s, %s, %s)"
+        cursor.execute(sql, (username, fullname, email, password))
+        user_id = cursor.lastrowid
+        #insertar en tabla informacion(me aseguro que usuario e informacion tengan el mismo id)
+        sql = "INSERT INTO informacion (usuario, informacion_adicional, image) VALUES (%s, %s, %s)"
+        cursor.execute(sql, (user_id,informacion_adicional,image))
+        conexion.connection.commit()
+        return jsonify({"mensaje": "Usuario registrado"})
+        
+    except Exception as ex:     # mostrar mensaje segun el tipo de error
+        print(f"Error: {ex}") 
+        return jsonify({"mensaje": "Error al registrar el usuario"})
+    
+    finally:
+        cursor.close()
+
+
+# ------- CRUD tabla usuarios -----------
+
+@app.route('/usuario', methods=["GET"])  #pedir a la bbdd informacion_adicional en esta peticion o en otra?
+def usuario():
+    username = request.args.get('username')
     try:
         cursor=conexion.connection.cursor()
-        sql = "SELECT * FROM usuarios WHERE nombre = %s AND apellido = %s"
-        cursor.execute(sql, (nombre, apellido))
+        sql = "SELECT * FROM usuarios WHERE username = %s "
+        cursor.execute(sql, (username,))
         datos=cursor.fetchone()
-        print(datos)
         if datos != None:
-            usuario = {"nombre":datos[1], "apellido":datos[2], "email": datos[3], "contraseña":datos[4]}
+            usuario = {"id":datos[0], "username":datos[1], "fullname":datos[2], "email": datos[3], "contraseña":datos[4]}
             return jsonify({"usuarios":usuario,"mensaje": "Datos del usuario"})
         else:
-            return jsonify({"mensaje": "Usuario no encontrado"})
-    
-    except Exception as ex:
-        return jsonify({"mensaje": "Error al buscar datos del usuario"})
+            return jsonify({"mensaje": "Usuario no encontrado"})    
+    except Exception as ex:    
+        return jsonify({"mensaje": "Error al buscar datos del usuario"})    
+    finally:
+        cursor.close()
 
-   
+@app.route('/usuarios_todos', methods=["GET"]) 
+def mostrar_todos_usuarios():
+    try:
+        cursor=conexion.connection.cursor()
+        sql = "SELECT * FROM usuarios"
+        cursor.execute(sql)
+        datos=cursor.fetchall()
+        usuarios = []
+        for user in datos:
+            usuario = {"id":user[0], "username":user[1], "fullname":user[2], "email": user[3], "contraseña":user[4]}
+            usuarios.append(usuario)
+        return jsonify({"usuarios":usuarios,"mensaje":"Todos los usuarios"})
+        
+    except Exception as ex:    
+        return jsonify({"mensaje": "Error al buscar datos del usuario"})    
+    finally:
+        cursor.close()
+
+@app.route('/usuario', methods=["PUT"])
+def actualizar_usuario():
+    id = request.args.get('id')
+    datos= request.get_json()
+    username = datos.get('username')  # Hacer que se pueda cambiar solo una vez por mes
+    fullname = datos.get('fullname')
+    email = datos.get('email')
+    password = datos.get('password')  #Hacer esta actualizacion en una peticion separada por seguridad
+    try: 
+        cursor=conexion.connection.cursor()
+        sql = "UPDATE usuarios SET username=%s, fullname=%s, email=%s, password=%s WHERE id=%s"
+        cursor.execute(sql, (username, fullname, email, password, id))
+        conexion.connection.commit()
+        return jsonify({"mensaje": "Datos actualizados"})        
+    except Exception as ex:
+        print(f"Error: {ex}") 
+        return jsonify({"mensaje": "Error al registrar el usuario"}), 500    
+    finally:
+        cursor.close()
+
+@app.route('/usuario', methods=["DELETE"])
+def borrar_usuario():
+    id = request.args.get('id')
+    try:
+        cursor=conexion.connection.cursor()
+        sql = "DELETE FROM usuarios WHERE id=%s"  #join al resto de las tablas del usuario y borrar tambien
+        cursor.execute(sql,(id,))
+        conexion.connection.commit()
+        return jsonify({"mensaje":"Has eliminado el usuario"})
+    except Exception as ex:
+        print(ex)
+        return jsonify({"mensaje":"error al eliminar el usuario"})
+    finally:
+        cursor.close()
+
+
+
+# ------- CRUD tabla informacion -----------
+
+@app.route('/informacion', methods=["PUT"])
+def actualizar_informacion():
+    id = request.args.get('id')
+    datos= request.get_json()
+    informacion = datos.get('informacion_adicional')
+    image = datos.get('image')    
+    try: 
+        cursor=conexion.connection.cursor()
+        sql = """UPDATE informacion 
+                INNER JOIN usuarios ON informacion.usuario = usuarios.id
+                SET informacion.informacion_adicional=%s, 
+                informacion.image=%s 
+                WHERE usuarios.id=%s"""
+        cursor.execute(sql, (informacion, image, id))
+        conexion.connection.commit()
+        return jsonify({"mensaje": "Datos actualizados"})        
+    except Exception as ex:
+        print(f"Error: {ex}") 
+        return jsonify({"mensaje": "Error al registrar el usuario"}), 500    
+    finally:
+        cursor.close()
+
+# ------- CRUD tabla perfiles    -----------
+# ------- CRUD tabla lenguajes   -----------
+
+
+
+
+
 def pagina_no_encontrada(error):
     return "<h1>La página no existe</h1>",404
 
